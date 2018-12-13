6874a3e248caa0a7ef28cdd29b9adb613c625f9c
yljvon <1689606211@qq.com>
2018-12-13 10:31:25 +0800

    第一次

---
+++ springcloud-api/src/main/java/com/springcloud/entity/Dept.java
@@ -0,0 +1,30 @@
+package com.springcloud.entity;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+
+@NoArgsConstructor
+//@AllArgsConstructor
+@Data
+@Accessors(chain=true)
+public class Dept implements Serializable// entity --orm--- db_table
+{
+	private Long 	deptno; // 主键
+	private String 	dname; // 部门名称
+	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同�?个信息被存储到不同数据库
+	
+	public Dept(String dname)
+	{
+		super();
+		this.dname = dname;
+	}
+	
+	
+}
